9158ddc07af7ed78872b4f41e79e24fd735fece8
josue <josuebelmont161@gmail.com>
2022-10-07 14:40:05 -0500

    first commit

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,46 @@
+
+
+<header class="shadow  p-3 md-5 bg-body rounded" style="width: 100%; z-index: 20; height: 90px;">
+    <nav class="navbar navbar-expand-lg bg">
+        <div class="container-fluid">
+            <div class="flex flex-row items-center">
+                <img src="/static/media/logo_jaliscosedefiende_small.0c5859dd.svg" alt="Ante la pandemia Jalisco se defiende" class="jalisco-gob pr-4 lg:pr-12 border-r">
+
+                <img src="/static/media/logo_jal_small.caf4e29b.svg" alt="Jalisco, Gobierno del estado" class="jalisco-se-defiende pl-4 sm:pl-12">
+
+            </div>
+            <div class="collapse navbar-collapse">
+            <ul class="navbar-nav">
+                <li class="nav-item"> <a href="https://calculatudosis.vacunacion.jalisco.gob.mx/" title="Calcula tu próxima dosis" class="nav-link" style="text-decoration: none;">Calcula tu próxima dosis</a></li>
+
+                <li class="nav-item"><a href="https://vacunacion.jalisco.gob.mx/static/media/AVISOS%20DE%20PRIVACIDAD.dec309d0.pdf" title="Aviso de privacidad" target="_blank" class="nav-link" rel="noreferrer" style="text-decoration: none;">Aviso de privacidad</a></li>
+            </ul>
+        </div>
+        </div>
+    </nav>
+
+    {{-- <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+              </li>
+              
+            </ul>
+          </div>
+        </div>
+      </nav> --}}
+
+    
+</header>
